06f176105a79c8c3757f509c2300de2f228a9705
korabieka <hussienashour@ymail.com>
2016-06-01 02:47:15 +0200

    maintaining

---
+++ IM/app/assets/javascripts/app.js
@@ -0,0 +1,32 @@
+angular.module('IM_module', ['ui.router','templates','Devise'])
+	.config([
+		'$stateProvider',
+		'$urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+
+		    $stateProvider
+			    .state('home', {
+			      url: '/home',
+			      templateUrl: 'home/_home.html',
+			  //     resolve: {
+					//   buildingPromise: ['buildings', function(buildings){
+					//   	console.log('hi') ;
+					//     return buildings.getAll();
+					//   }]
+					// },
+
+			      controller: 'MainCtrl',
+			      controllerAs: "homeCtrl" ,
+			      
+			    })
+			   .state('buildings', {
+				  url: '/buildings/{id}',
+				  templateUrl: 'buildings/_buildings.html',
+				  controller: 'buildingsCtrl' ,
+				  controllerAs: "buildCtrl"
+				});
+
+		    $urlRouterProvider.otherwise('home');
+	}])
+
+
