d66de70ae0dcdba04ae8aa81e6018f942fd0b99a
Mohamed ayyad <moh.ayad93@hotmail.com>
2016-06-05 12:32:22 +0200

    update in blocks

--- IM/app/assets/javascripts/blocks/blocksCtrl.js
+++ IM/app/assets/javascripts/blocks/blocksCtrl.js
@@ -6,38 +6,12 @@ angular.module('IM_module')
 			console.log($stateParams.floor_id);
 	
 			var self = this;
-			
-			self.editBlock = {};
 			self.isEdit = false;
-			// self.editBlock.name = self.block.name ;
-
+			
 			blocks.getOne($stateParams.building_id,$stateParams.floor_id,$stateParams.id).success(function(data){
 		    	console.log(data);
-				// self.build = data ;
 		    });
 
 
-			self.submiteditBlockForm = function(buildId , floorId){
-		    	console.log("submit") ;
-		    	console.log(self.editBlock) ;
-		    	// console.log(self.editId) ;
-		    	// console.log(buildId);
-		    	// console.log(floorId);
-		    	blocks.editOne(buildId , floorId, self.editId , self.editBlock).success(function(data){
-			    	console.log(data);
-			    	self.block.name = data.name;
-			    	console.log(self.block);
-		    	});
-		    	self.isEdit = false ;
-			   	
-		    }
-
 			
-			self.editFunc = function(block){
-				console.log(block.name);
-		    	self.isEdit = true;
-		    	self.editId = block.id;
-		    	self.editBlock.name = block.name ;
-		    	self.editBlock.id = block.id;
-			 }
 	}]);
