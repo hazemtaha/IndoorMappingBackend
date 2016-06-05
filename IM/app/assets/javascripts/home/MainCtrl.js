fdc4e7db3d498a1cb155c9eda396679c41befcf6
Mohamed ayyad <moh.ayad93@hotmail.com>
2016-06-05 13:08:43 +0200

    fix ajax reply

--- IM/app/assets/javascripts/home/MainCtrl.js
+++ IM/app/assets/javascripts/home/MainCtrl.js
@@ -2,6 +2,7 @@ angular.module('IM_module')
 	.controller('MainCtrl', ['buildings',
 		function(buildings){
 			var self = this;
+			self.addBuild = {};
 			buildings.getAll().success(function(data){
 				self.buildings = data ;
 		    })
@@ -10,6 +11,28 @@ angular.module('IM_module')
 		    		self.buildings = data ;
 	    		})
 		    }
+
+
+		    self.submitaddBuildForm = function(){
+		    	//self.addBuild.building_id = self.build.id ;
+		    	//console.log(self.addFloor) ;
+		    	buildings.addOne(self.addBuild).success(function(data){
+		    		if (data.errorMsg !== undefined ){
+		    			self.buildExist = data.errorMsg ;
+		    			console.log(data.errorMsg);
+		    			console.log('errrrrrrrrrrrr');		
+		    		}else{
+		    			console.log('no errrrrrrrrrrrr');
+		    			console.log(data);
+		    			self.buildings = data ;	
+					    $('.modal').modal('hide');
+		    		}
+		    	});
+
+		    	self.addBuild = {} ;
+		    	self.addBuildForm.$setPristine();
+		    }	
+
 	}])
 
 	
