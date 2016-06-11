2760d68c166c13348903735d7da857f4eaedbd28
korabieka <hussienashour@ymail.com>
2016-06-11 05:36:43 +0200

    modifing visitors table to be handled by devise gem

--- IM/app/models/visitor.rb
+++ IM/app/models/visitor.rb
@@ -1,3 +1,8 @@
 class Visitor < ActiveRecord::Base
+# Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :beacons, through: :visits
+
 end
