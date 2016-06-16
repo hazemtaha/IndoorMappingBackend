49bba7aac502cbee067ed2f5ce9d0ea1b2a61f53
Ghada <blackhourse.112014@gmail.com>
2016-06-16 14:00:21 +0200

    map button,display current user info

--- IM/app/assets/javascripts/nav/navCtrl.js
+++ IM/app/assets/javascripts/nav/navCtrl.js
@@ -1,23 +1,32 @@
 angular.module('IM_module')
-.controller('NavCtrl', [
-'Auth',
-function(Auth){
-	this.signedIn = Auth.isAuthenticated;
-  	this.logout = Auth.logout;
-  	 Auth.currentUser().then(function (user){
-     this.user = user;
-     this.$on('devise:new-registration', function (e, user){
-      this.user = user;
-  	});
+  .controller('NavCtrl', [
+  'Auth',
+  function(Auth){
+    	  this.signedIn = Auth.isAuthenticated;
+      	this.logout = Auth.logout;
+        this.cu = "anahenA" ;
+        var self =this ;
+      	Auth.currentUser().then(function(user){
+            self.user = user;
+            console.log(user) ;
+           //  this.$on('devise:new-registration', function (e, user){
+           //    this.user = user;
+          	// });
 
-  this.$on('devise:login', function (e, user){
-    this.user = user;
-    console.log(e) ;
-    console.log("dfh") ;
-  });
+           //  this.$on('devise:login', function (e, user){
+           //    this.user = user;   
+           //    console.log(e) ;
+           //    console.log("dfh") ;
+           //  });
 
-  this.$on('devise:logout', function (e, user){
-    this.user = {};
-  });
-  });
-}]);
+           //  this.$on('devise:logout', function (e, user){
+           //    this.user = {};
+           //  });
+        }
+        ,function(data){
+            console.log('error') ;
+            console.log(data) ;
+          }
+        );
+
+  }]);
