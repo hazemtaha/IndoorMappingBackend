852f3b90e3b6edaa73355787e976e23a14e6ce56
Hazem Taha <thatskan@gmail.com>
2016-06-16 09:34:59 +0200

    fix delete floor request

--- IM/app/assets/javascripts/floors/floors.js
+++ IM/app/assets/javascripts/floors/floors.js
@@ -1,13 +1,13 @@
 angular.module('IM_module')
 	.factory('floors', ['$http' , function($http){
 
-       var obj = {}; 
+       var obj = {};
 
 		obj.getOne = function(bid,fid) {
-		   return $http.get('/buildings/'+bid+'/floors/'+fid+'.json'); 
+		   return $http.get('/buildings/'+bid+'/floors/'+fid+'.json');
 		};
 		obj.delOne = function(bid,fid) {
-		   return $http.delete('/buildings/'+bid+'/floors/'+fid+'json'); 
+		   return $http.delete('/buildings/'+bid+'/floors/'+fid+'.json'); 
 		};
 		obj.addOne = function(bid,addFloor) {
 			console.log('inside add floor') ;
@@ -16,7 +16,6 @@ angular.module('IM_module')
 		    return $http.post('/buildings/'+bid+'/floors.json', addFloor );
 		};
 
-		return obj ; 
+		return obj ;
 
 	}])
-
