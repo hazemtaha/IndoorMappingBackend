65de6d9478e9a3d9e311c74075e3e8253b38e8dd
Hazem Taha <thatskan@gmail.com>
2016-06-16 14:37:06 +0200

    update visit create action

--- IM/app/controllers/visit_controller.rb
+++ IM/app/controllers/visit_controller.rb
@@ -1,6 +1,6 @@
 class VisitController < ApplicationController
   def create
-    @beacn_id = Beacon.find_by(uuid: params[:uuid]).id
+    @beacon_id = Beacon.find_by(uuid: params[:uuid]).id
     Visit.create(visitor_id: params[:visitor_id], beacon_id: @beacon_id)
   end
 end
