8aaea550f2843bd539349989497d14c41f2cf896
Hazem Taha <thatskan@gmail.com>
2016-06-18 15:41:53 +0200

    update broadcast after registeration

--- IM/app/assets/javascripts/nav/navCtrl.js
+++ IM/app/assets/javascripts/nav/navCtrl.js
@@ -1,27 +1,28 @@
 angular.module('IM_module')
   .controller('NavCtrl', [
   'Auth',
-  function(Auth){
-    	  this.signedIn = Auth.isAuthenticated;
-      	this.logout = Auth.logout;
-        this.cu = "anahenA" ;
-        var self =this ;
+  '$rootScope',
+  function(Auth, $rootScope){
+    var self = this ;
+    	  self.signedIn = Auth.isAuthenticated;
+      	self.logout = Auth.logout;
       	Auth.currentUser().then(function(user){
             self.user = user;
             console.log(user) ;
-           //  this.$on('devise:new-registration', function (e, user){
-           //    this.user = user;
-          	// });
+            $rootScope.$on('devise:new-registration', function (e, user){
+              self.user = user;
+          	});
 
-           //  this.$on('devise:login', function (e, user){
-           //    this.user = user;   
-           //    console.log(e) ;
-           //    console.log("dfh") ;
-           //  });
+            $rootScope.$on('devise:login', function (e, user){
+              self.user = user;
+              console.log(e) ;
+              console.log("dfh") ;
+            });
 
-           //  this.$on('devise:logout', function (e, user){
-           //    this.user = {};
-           //  });
+            $rootScope.$on('devise:logout', function (e, user){
+              console.log(user);
+              self.user = {};
+            });
         }
         ,function(data){
             console.log('error') ;
